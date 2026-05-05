5a0b8b106a709f62ed35692f4775a7b569b31312
EndeavorSeeker <morad.amqour.edu@gmail.com>
2026-05-06 00:11:17 +0100

    Added aymen work files

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/safe')
+def safe():
+    return render_template('safe.html')
+@app.route('/threat')
+def threat():
+    return render_template('threat.html')
+@app.route('/img')
+def image():
+    return render_template('image.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
